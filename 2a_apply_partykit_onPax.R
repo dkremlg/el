22a8694c8769a957c603d8a81cc4fc8a61ed8113
dkremlg <kochdaniel@pt.lu>
2019-05-03 01:19:00 +0200

    Commit of home office

--- 2a_apply_partykit_onPax.R
+++ 2a_apply_partykit_onPax.R
@@ -1,8 +1,8 @@
 library(partykit)
 
-path='/home/ubuntu/Data/Intermediate_Output/'
+path='C:/Users/35266/Documents/Python Scripts/el/'
 
-Data=read.csv(paste0(path,'R_Training_Pax.csv'))
+Data=read.csv(paste0(path,'Intermediate_Output/R_Training_Pax.csv'))
 Data[Data[,'NumPax']<0,'NumPax']=0
 
 class(eq.mob <- NumPax ~ 1 + Dprio | dday + dtime + Direction + month)  
@@ -25,10 +25,10 @@ curve.model <- glmtree(
 forecast_bookings=as.numeric(predict(curve.model,newdata=Data,type='response'))
 forecast_node=as.numeric(predict(curve.model,newdata=Data,type='node'))
 Data=cbind(Data,forecast_bookings,forecast_node)
-write.csv(Data,paste0(path,'R_Output_Training_Pax.csv'),row.names=FALSE)
+write.csv(Data,paste0(path,'Intermediate_Output/R_Output_Training_Pax.csv'),row.names=FALSE)
 
-Forecast_Data=read.csv(paste0(path,'R_Test_Pax.csv'))
+Forecast_Data=read.csv(paste0(path,'Intermediate_Output/R_Test_Pax.csv'))
 forecast_bookings=as.numeric(predict(curve.model,newdata=Forecast_Data,type='response'))
 forecast_node=as.numeric(predict(curve.model,newdata=Forecast_Data,type='node'))
 Forecast_Data=cbind(Forecast_Data,forecast_bookings,forecast_node)
-write.csv(Forecast_Data,paste0(path,'R_Output_Test_Pax.csv'),row.names=FALSE)
+write.csv(Forecast_Data,paste0(path,'Intermediate_Output/R_Output_Test_Pax.csv'),row.names=FALSE)
